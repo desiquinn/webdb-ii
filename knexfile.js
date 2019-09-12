ccf7584d46b24f05845a8230c6203d61c7c8168e
Desiree Morris <desiquinncompany@gmail.com>
2019-09-12 09:01:42 -0400

    formatted the knexfile

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,10 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/car-dealer.db3'
+    },
+    useNullAsDefault: true
   },
+
+  
 };
